3d8fffaa9ebe79693a251b4256d165398ee6343d
Karan G <karangb@gmail.com>
2013-01-06 03:01:26 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripAdvisor::Application.config.secret_token = 'f895b288b8cd7d65f1a554964d17dd600d85222a0bcdfb0ccbb0255ad580d38b296598843f95ce4415df22766082e6d91a41b2fc40aed64044497e38713ef60d'
